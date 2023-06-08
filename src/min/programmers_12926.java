f85c26182bcae2c7634e9318a281242c50b0c6ad
min-gui <msj349911@gmail.com>
2023-06-08 21:22:29 +0900

    민섭 첫번째 커밋

---
+++ src/min/programmers_12926.java
@@ -0,0 +1,23 @@
+package min;
+
+public class programmers_12926 {
+    public static void main(String[] args) {
+        String s = "a B z";
+        solution(s, 4);
+    }
+
+    public static String solution(String s, int n) {
+        StringBuilder stringBuilder = new StringBuilder();
+        char[] arr = s.toCharArray();
+        for (char c : arr) {
+            if (c == ' ' ){
+                stringBuilder.append(" ");
+            }else if(c >= 'a' && c <= 'z'){
+                stringBuilder.append(Character.toString((((c-97)+n)%26)+97));
+            }else {
+                stringBuilder.append(Character.toString((((c-65)+n)%26)+65));
+            }
+        }
+        return stringBuilder.toString();
+    }
+}
