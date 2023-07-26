09c31bd863bcfbc5a7b46b58adf3481b187755d0
min-gui <msj349911@gmail.com>
2023-07-26 21:19:24 +0900

    민섭 81301

---
+++ src/ch4/pb12/Programmers_min_81301.java
@@ -0,0 +1,27 @@
+package ch4.pb12;
+
+public class Programmers_min_81301 {
+
+    public static void main(String[] args) {
+        Programmers_min_81301 solution = new Programmers_min_81301();
+        System.out.println(solution.solution("one4seveneight"));
+    }
+
+    //문제 ㅈ 같네
+    public int solution(String s) {
+        int answer = 0;
+        s = s.replaceAll("zero", "0");
+        s = s.replaceAll("one", "1");
+        s = s.replaceAll("two", "2");
+        s = s.replaceAll("three", "3");
+        s = s.replaceAll("four", "4");
+        s = s.replaceAll("five", "5");
+        s = s.replaceAll("six", "6");
+        s = s.replaceAll("seven", "7");
+        s = s.replaceAll("eight", "8");
+        s = s.replaceAll("nine", "9");
+        answer = Integer.parseInt(s);
+        return answer;
+    }
+
+}
