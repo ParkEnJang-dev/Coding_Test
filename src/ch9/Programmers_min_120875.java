e0d92c38dacc3d178934c1c62d0ecdc634017f88
min-gui <msj349911@gmail.com>
2023-09-26 23:11:33 +0900

    수정

--- src/ch9/Programmers_min_120875.java
+++ src/ch9/Programmers_min_120875.java
@@ -7,23 +7,24 @@ public class Programmers_min_120875 {
 
     public static void main(String[] args) {
         Programmers_min_120875 solution = new Programmers_min_120875();
-        int solution1 = solution.solution(new int[][]{{1, 4}, {9, 2}, {3, 8}, {11, 6}});
+//        int solution1 = solution.solution(new int[][]{{1, 4}, {9, 2}, {3, 8}, {11, 6}});
 //        int solution1 = solution.solution(new int[][]{{3, 5}, {4, 1}, {2, 4}, {5, 10}});
+        int solution1 = solution.solution(new int[][]{{1, 1}, {2, 2}, {3, 3}, {50, 100}});
         System.out.println(solution1);
     }
 
     public int solution(int[][] dots) {
-        Set<Double> set = new HashSet<>();
+        Set<Double> hashSet = new HashSet<>();
         for (int i = 0; i < dots.length; i++) {
             for (int j = i + 1; j < dots.length; j++) {
-                int x = Math.abs(dots[i][0] - dots[j][0]);
-                int y = Math.abs(dots[i][1] - dots[j][1]);
+                int x = dots[j][0] - dots[i][0];
+                int y = dots[j][1] - dots[i][1];
 
-                if (set.contains((double)y / x)) {
+                if (hashSet.contains((double)x / y)) {
                     return 1;
                 }
 
-                set.add((double)y / x);
+                hashSet.add((double)x / y);
             }
         }
 
