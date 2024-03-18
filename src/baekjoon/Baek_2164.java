1c1464946259b4782fad9779591227555e8eb5b9
min-gui <msj349911@gmail.com>
2024-03-18 22:54:53 +0900

    baek 2164

---
+++ src/baekjoon/Baek_2164.java
@@ -0,0 +1,29 @@
+package baekjoon;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Baek_2164 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int N = sc.nextInt();
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 1 ; i <= N; i++){
+            queue.add(i);
+        }
+
+        while (true){
+
+            if (queue.size() == 1){
+                System.out.println(queue.poll() );
+                break;
+            }
+            queue.poll();
+            queue.add(queue.poll());
+        }
+
+    }
+}
